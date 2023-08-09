b77bdb1ffedfa9e35389a9efed5718d2b28ee8bf
Pratham Madaram <90805743+BEEB0SS@users.noreply.github.com>
2023-08-08 23:40:12 -0400

    changing_prompt

--- AI_Therapist_main.py
+++ AI_Therapist_main.py
@@ -1,12 +1,14 @@
 import openai
 import gradio
 
-openai.api_key = "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"
+openai.api_key = "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"
 
-messages = [{"role": "system", "content": "You are a psychologist"}]
+messages = [{"role": "system", "content": "You are a psychologist and therapist with 40+ years of experience. Your job is to utilize the techniques you've learnt over the years to make your patients feel better. These techniques include but are not limited to appropriate use of silence, rephrasing or paraphrasing, reflection, summarizing, and acknowledgement"}]
 
-def CustomChatGPT(user_input):
-    messages.append({"role": "user", "content": user_input})
+
+
+def Therapist(patient):
+    messages.append({"role": "user", "content": patient})
     response = openai.ChatCompletion.create(
         model = "gpt-3.5-turbo",
         messages = messages
@@ -15,6 +17,6 @@ def CustomChatGPT(user_input):
     messages.append({"role": "assistant", "content": ChatGPT_reply})
     return ChatGPT_reply
 
-demo = gradio.Interface(fn=CustomChatGPT, inputs = "text", outputs = "text", title = "Albert The AI Therapist")
+therapist = gradio.Interface(fn=Therapist, inputs = "text", outputs = "text", title = "Albert The AI Therapy Advisor")
 
-demo.launch(share=True)
+therapist.launch(share=True)
