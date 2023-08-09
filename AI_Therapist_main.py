f0dad72d75fb41d1c991ce41c7f678b6cf0cb2b2
Pratham Madaram <90805743+BEEB0SS@users.noreply.github.com>
2023-08-09 01:35:54 -0400

    Update AI_Therapist_main.py

--- AI_Therapist_main.py
+++ AI_Therapist_main.py
@@ -1,7 +1,10 @@
 import openai
 import gradio
+import os
 
-openai.api_key = "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"
+
+openai.api_key = os.environ["api_key"]
+
 
 messages = [{"role": "system", "content": "You are a psychologist and therapist with 40+ years of experience. Your job is to utilize the techniques you've learnt over the years to make your patients feel better. These techniques include but are not limited to appropriate use of silence, rephrasing or paraphrasing, reflection, summarizing, and acknowledgement"}]
 
@@ -17,6 +20,7 @@ def Therapist(patient):
     messages.append({"role": "assistant", "content": ChatGPT_reply})
     return ChatGPT_reply
 
-therapist = gradio.Interface(fn=Therapist, inputs = "text", outputs = "text", title = "Albert The AI Therapy Advisor")
+therapist = gradio.Interface(fn=Therapist, inputs = "text", outputs = "text", title = "Albert The AI Therapist")
 
-therapist.launch(share=True)
+#therapist.launch(share=True)
+therapist.launch()
